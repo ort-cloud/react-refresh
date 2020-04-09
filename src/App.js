f2aad11d871500a50a8c9deeef203c6ee5b68113
Carl Ortutay <carl.orty@gmail.com>
2020-04-09 13:17:45 -0400

    added GoalList file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './App.css'
+import React from "react";
+import "./App.css";
+import GoalList from "./components/GoalList";
 
 const App = () => {
-  return <div className="course-goals">
-    <h2>Course Goals</h2>
-    <ul className="goal-list">
-      <li>Finish the Course</li>
-      <li>Learn all about the course main topic</li>
-      <li>Help other students in the course Q & A</li>
-    </ul>
-  </div>
+  return (
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/GoalList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GoalList = () => {
+  return (
+    <ul className='goal-list'>
+      <li>Finish the Course</li>
+      <li>Learn all about the course main topic</li>
+      <li>Help other students in the course Q & A</li>
+    </ul>
+  );
+};
+
+export default GoalList;
