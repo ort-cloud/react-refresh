f53ed0e9b8f32322dba65f5df602f47d51d11575
Carl Ortutay <carl.orty@gmail.com>
2020-04-09 13:56:22 -0400

    Goal list now dynamic

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './GoalList.css'
 
-const GoalList = () => {
+const GoalList = props => {
   return (
     <ul className='goal-list'>
-      <li>Finish the Course</li>
-      <li>Learn all about the course main topic</li>
-      <li>Help other students in the course Q & A</li>
+      {props.goals.map(goal => {
+        return <li key={goal.id}>{goal.text}</li>
+      })}
     </ul>
   );
 };
